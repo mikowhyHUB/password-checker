d1260b18479ee4a89755e1a2e27b6e9178422ee7
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-10-25 17:14:32 +0200

    new features

--- check_my_pass.py
+++ check_my_pass.py
@@ -1,5 +1,26 @@
+import hashlib
 import requests
+import hashlib
 
-url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
-res = requests.get(url)
-print(res)
+
+def request_ap_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError('Error fetching')
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text)
+
+
+def pwned_api_check(password):
+    # check password if it exists in API
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_ap_data(first5_char)
+    return response
+
+
+pwned_api_check('123')
